1c1a525652fbf7c3fc32ebfcd4e9a1deaf757725
rodesta2212 <rodesta2212@gmail.com>
2022-01-03 13:27:54 +0700

    hidden logo deksapp and github

--- head-navbar.php
+++ head-navbar.php
@@ -130,7 +130,7 @@
 				</div>
 			</div>
 			<div class="github-link">
-				<a href="https://github.com/dropways/deskapp" target="_blank"><img src="vendors/images/github.svg" alt=""></a>
+				<!-- <a href="https://github.com/dropways/deskapp" target="_blank"><img src="vendors/images/github.svg" alt=""></a> -->
 			</div>
 		</div>
 	</div>
